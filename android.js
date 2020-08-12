2ec987ffd25a33b165fb6016c5d6e60a3ee6ddfd
Aswani Gedela <gakhila281994@gmail.com>
2020-08-12 20:09:51 +0530

    Question Files

---
+++ android.js
@@ -0,0 +1,10 @@
+// Android Development Questions
+const questions = [
+    { 'question': 'Android is Developed By?', 'options': [ "Apple", "Microsoft", "Google", "Android Inc" ], 'answer': "Android Inc" },
+    { 'question': 'Which Media Format is Not supported by Android?', 'options': [ "MP4", "AVI", "MIDI", "MPEG" ], 'answer': "AVI" },
+    { 'question': 'Which Programming Language is Used For Android Application Development?', 'options': [ "NodeJS", "PHP", "JSX", "Java" ], 'answer': 'Java' },
+    { 'question': 'Adb Stands For?', 'options': [ "Android Debug Bridge.", "Android Drive Bridge.", "Android Delete Bridge", "Android Destroy Bridge." ], 'answer': 'Android Debug Bridge.' },
+    { 'question': 'Android Web Browser Is Based On?', 'options': [ "Chrome", "Open-source Webkit", "Safari", "Firefox" ], 'answer': 'Open-source Webkit' },
+]
+
+export default questions
---
+++ web.js
@@ -0,0 +1,10 @@
+// Web Designing Questions
+const questions = [
+    { 'question': '…………. requires extra download round-trip for the style sheet, which might delay page rendering, particularly when multiple files are in use.?', 'options': [ "External Style Sheets", "Document Wide Style", "Inline Style", "All of the above" ], 'answer': "External Style Sheets" },
+    { 'question': 'Which of the following is/are the sub properties of background property in CSS?', 'options': [ "background-color", "background-image", "background-attachment", "All of the above" ], 'answer': "All of the above" },
+    { 'question': 'The CSS selector E>F selects …?', 'options': [ "direct decedents", "adjacent siblings", "preceding siblings", "all elements" ], 'answer': 'direct decedents' },
+    { 'question': 'An ……… rule can be used to define style rules for multiple media types in a single embedded style sheet.?', 'options': [ "@embedded", "@media", "@page", "@multiple" ], 'answer': '@media' },
+    { 'question': 'The different ways to associate styles with a HTML document is/are?', 'options': [ "Embedded CSS with <style> element", "Inline CSS with style attribute.", "External CSS with <link> element.", "All of the above" ], 'answer': 'All of the above' },
+]
+
+export default questions
