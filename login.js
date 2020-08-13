ac3ccad05b22fc65ab57807bc10ec0ffd8550460
Aswani Gedela <gakhila281994@gmail.com>
2020-08-13 08:41:36 +0530

    Added URL SearchParams

--- login.js
+++ login.js
@@ -1,49 +1,53 @@
-window.onload = function(){
-    document.querySelector('body').style.opacity = 1
-    var creatAccountBtn = document.getElementById('createAccount')
-    creatAccountBtn.addEventListener('click', goToRegistrationPage)
+window.onload = function () {
+    document.querySelector( 'body' ).style.opacity = 1
+    var creatAccountBtn = document.getElementById( 'createAccount' )
+    creatAccountBtn.addEventListener( 'click', goToRegistrationPage )
 
-    var signInBtn = document.querySelector('form')
-    signInBtn.addEventListener('submit', getUserData )
+    var signInBtn = document.querySelector( 'form' )
+    signInBtn.addEventListener( 'submit', getUserData )
 }
 
-function goToRegistrationPage(){
+function goToRegistrationPage () {
     //this function creates a fade in transition effect
-    document.querySelector('body').style.opacity = 0
-    setTimeout(function(){
+    document.querySelector( 'body' ).style.opacity = 0
+    setTimeout( function () {
         window.location.href = 'index.html'
-    },500)
+    }, 500 )
 }
 
-function getUserData(){
+function getUserData () {
     event.preventDefault()
-    var allUserData = event.target.querySelectorAll('input')
+    var allUserData = event.target.querySelectorAll( 'input' )
 
     var data = {
-        username: allUserData[0].value,
-        password: allUserData[1].value
+        username: allUserData[ 0 ].value,
+        password: allUserData[ 1 ].value
     }
 
-    var validate = validateData(data)
-    if(validate){
+    var validate = validateData( data )
+    if ( validate ) {
         var activeUser = {}
-        activeUser['username'] = allUserData[0].value
-        activeUser['selectedCourse'] = validate
-        localStorage.setItem('activeUser',JSON.stringify(activeUser))
+        activeUser[ 'username' ] = allUserData[ 0 ].value
+        activeUser[ 'selectedCourse' ] = validate
         // go to Dashboard
-        window.location.href = 'userDashBoard.html'
+        let params = new URLSearchParams()
+        params.append( 'user', activeUser.username )
+        params.append( 'course', activeUser.selectedCourse )
+        console.log( params.toString() );
+        // window.location.href = ''
+        window.location.assign( `userDashBoard.html?${ params.toString() }` )
     }
-    else{
+    else {
         // show error
-        $('#errorModal').modal('show')
+        $( '#errorModal' ).modal( 'show' )
     }
 }
 
-function validateData(data){
-    var localData = JSON.parse(localStorage.getItem('userRegistrationDetails'))
-    for(var i = 0; i < localData.length; i++){
-        if(localData[i]['username'] == data.username && localData[i]['password'] == data.password){
-            let userSelectedCourse = localData[i].selectedCourse
+function validateData ( data ) {
+    var localData = JSON.parse( localStorage.getItem( 'userRegistrationDetails' ) )
+    for ( var i = 0; i < localData.length; i++ ) {
+        if ( localData[ i ][ 'username' ] == data.username && localData[ i ][ 'password' ] == data.password ) {
+            let userSelectedCourse = localData[ i ].selectedCourse
             return userSelectedCourse
         }
     }
--- userDashBoard.js
+++ userDashBoard.js
@@ -1,26 +1,27 @@
-window.addEventListener('DOMContentLoaded',()=>{
-    //get the name of the user from local storage and display in the application bar
-    const userName = JSON.parse(localStorage.getItem('activeUser')).username.toUpperCase()
-    const selectedCourse = JSON.parse(localStorage.getItem('activeUser')).selectedCourse
-    let userNameDiv = document.getElementById('userName')
+window.addEventListener( 'DOMContentLoaded', () => {
+    //get the name of the user from URL and display in the application bar
+    let user = new URLSearchParams( window.location.search )
+    const userName = user.get( 'user' ).toUpperCase()
+    const selectedCourse = user.get( 'course' )
+    let userNameDiv = document.getElementById( 'userName' )
     userNameDiv.textContent = userName
 
     //display the card based on the selected course
-    displayCard(selectedCourse)
+    displayCard( selectedCourse )
 
-})
+} )
 
-displayCard=(selectedCourse)=>{
-    if(selectedCourse == 'Web Design'){
-        let card = document.querySelector('.webDesignCard')
+displayCard = ( selectedCourse ) => {
+    if ( selectedCourse == 'Web Design' ) {
+        let card = document.querySelector( '.webDesignCard' )
         card.classList = 'col webDesignCard'
     }
-    else if(selectedCourse == 'App Development'){
-        let card = document.querySelector('.appDevelopmentCard')
+    else if ( selectedCourse == 'App Development' ) {
+        let card = document.querySelector( '.appDevelopmentCard' )
         card.classList = 'col appDevelopmentCard'
     }
-    else if(selectedCourse == 'Digital Marketing'){
-        let card = document.querySelector('.digitalMarketingCard')
+    else if ( selectedCourse == 'Digital Marketing' ) {
+        let card = document.querySelector( '.digitalMarketingCard' )
         card.classList = 'col digitalMarketingCard'
     }
 }
